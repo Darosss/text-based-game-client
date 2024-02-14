22c617dce4c0bafdf9eaefa6d71e5c3bb8cf4a2f
Darosss <daroing12@gmail.com>
2024-02-14 23:25:33 +0100

    feat: add BaseDetails to CharacterStatistics

--- src/components/characters/characters/character-statistics.tsx
+++ src/components/characters/characters/character-statistics.tsx
@@ -1,3 +1,4 @@
+import { Character } from "@/api/types";
 import { AdditionalStatistics, BaseStatistics } from "../details/statistics";
 import { useCharacterManagementContext } from "./character-management-context";
 import styles from "./character-statistics.module.scss";
@@ -12,8 +13,40 @@ export const CharacterStatistics = () => {
   if (!data) return <></>;
   return (
     <div className={styles.characterStatistics}>
+      <BaseDetails character={data} />
       <BaseStatistics statistics={data.stats.statistics} />
       <AdditionalStatistics statistics={data.stats.additionalStatistics} />
+    </div>
+  );
+};
+
+type BaseDetailsProps = {
+  character: Character;
+};
+
+const BaseDetails = ({
+  character: { name, level, health, experience, expToLevelUp },
+}: BaseDetailsProps) => {
+  return (
+    <div className={styles.baseDetails}>
+      <div>
+        <div>Name</div>
+        <div> {name}</div>
+      </div>
+      <div>
+        <div>Level </div>
+        <div>{level}</div>
+      </div>
+      <div>
+        <div>Health points</div>
+        <div>{health}</div>
+      </div>
+      <div>
+        <div>Experience </div>
+        <div>
+          {experience} / {expToLevelUp}
+        </div>
+      </div>
     </div>
   );
 };
