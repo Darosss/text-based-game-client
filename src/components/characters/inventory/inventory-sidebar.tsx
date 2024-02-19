07d17a57819591511c3f9c452d074b67170c16f6
Darosss <daroing12@gmail.com>
2024-02-19 10:10:56 +0100

    feat: add inventory sidebar placeholders images

--- src/components/characters/inventory/inventory-sidebar.tsx
+++ src/components/characters/inventory/inventory-sidebar.tsx
@@ -2,13 +2,13 @@ import { ItemType } from "@/api/enums";
 import styles from "./inventory-sidebar.module.scss";
 import { Button } from "@/components/common/button";
 import { useInventoryControlContext } from "./inventory-control-context";
+import Image from "next/image";
 export const InventorySidebar = () => {
   const { filter, setFilter } = useInventoryControlContext();
   return (
     <div className={styles.sidebarWrapper}>
       {Object.values(ItemType).map((type) => {
         const isChoosen = filter?.showType?.includes(type);
-        //TODO: add images icons. for now font size smaller.
         return (
           <Button
             key={type}
@@ -32,7 +32,12 @@ export const InventorySidebar = () => {
               });
             }}
           >
-            {type}
+            <Image
+              src={`/images/inventory/navigation/${type.toLowerCase()}.png`}
+              sizes="(max-width: 768px) 25vw, (max-width: 1200px) 50vw, 50vw"
+              alt="item"
+              fill
+            />
           </Button>
         );
       })}
