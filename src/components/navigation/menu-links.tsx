4569a2d8b971377b3efa756109a96e74ea885182
Darosss <daroing12@gmail.com>
2024-02-23 16:04:21 +0100

    Revert "feat: add links visiblity for logged and logged off users"
    This reverts commit b48a589ce724405b315e9cc846a1303aba306e0b.

--- src/components/navigation/menu-links.tsx
+++ src/components/navigation/menu-links.tsx
@@ -1,49 +1,35 @@
-import Cookies from "js-cookie";
 import Link from "next/link";
 import styles from "./menu-links.module.scss";
-import { COOKIE_TOKEN_NAME } from "@/api/fetch";
 
 export const MenuLinks: () => JSX.Element = () => {
-  const authToken = Cookies.get(COOKIE_TOKEN_NAME);
   return (
     <ul className={styles.menuLinksList}>
-      {authToken ? (
-        <>
-          <li>
-            <Link href="/overview">
-              <p>Overview</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/skirmishes">
-              <p>Skirmishes</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/dungeons">
-              <p>Dungeons</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="">
-              <p>Logout</p>
-            </Link>
-          </li>
-        </>
-      ) : (
-        <>
-          <li>
-            <Link href="/auth/login">
-              <p>Login</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/auth/register">
-              <p>Register</p>
-            </Link>
-          </li>
-        </>
-      )}
+      <li>
+        <Link href="/auth/login">
+          <p>Login</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/auth/register">
+          <p>Register</p>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/overview">
+          <p>Overview</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/skirmishes">
+          <p>Skirmishes</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/dungeons">
+          <p>Dungeons</p>
+        </Link>
+      </li>
     </ul>
   );
 };
