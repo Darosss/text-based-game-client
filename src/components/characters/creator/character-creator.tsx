388635c1ebfce2baf8a194d1fcaa2b2d3b4f2566
Darosss <daroing12@gmail.com>
2024-02-23 23:14:43 +0100

    feat: add CharacterCreator component

---
+++ src/components/characters/creator/character-creator.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useFetch } from "@/hooks/useFetch";
+import styles from "./character-creator.module.scss";
+import { Button } from "@/components/common/button";
+import { useEffect } from "react";
+import { useCharacterManagementContext } from "../characters/character-management-context";
+
+/* TODO: 
+here i need to add later:
+- character profession
+- character sex
+etc, itp
+for now it's just create button 
+
+*/
+
+export const CharacterCreator: () => JSX.Element = () => {
+  const {
+    apiCharacter: { fetchData: fetchCharacterData },
+  } = useCharacterManagementContext();
+
+  const {
+    api: { responseData },
+    fetchData,
+  } = useFetch(
+    { url: "characters/create", method: "POST" },
+    {
+      manual: true,
+      notification: { pendingText: "Trying to create main character" },
+    }
+  );
+
+  useEffect(() => {
+    if (responseData.data) fetchCharacterData();
+  }, [responseData.data, fetchCharacterData]);
+
+  return (
+    <div className={styles.characterCreatorWrapper}>
+      <h1>Char creator</h1>
+
+      <div>
+        <Button onClick={fetchData}>Create main character</Button>
+      </div>
+    </div>
+  );
+};
