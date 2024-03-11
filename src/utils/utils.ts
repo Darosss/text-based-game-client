90c8b56bb9f178694f0d029fe9f5b352b6948949
Darosss <daroing12@gmail.com>
2024-03-11 18:36:23 +0100

    feat(utils): add days to formatTime

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -16,12 +16,14 @@ export const formatTime = (milliseconds: number): string => {
   const seconds = Math.floor(milliseconds / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
 
+  const formattedDays = days > 0 ? days + "d" : "";
   const formattedHours = hours > 0 ? padWithZero(hours % 24) + ":" : "";
   const formattedMinutes = padWithZero(minutes % 60);
   const formattedSeconds = padWithZero(seconds % 60);
 
-  return `${formattedHours}${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedDays} ${formattedHours}${formattedMinutes}:${formattedSeconds}`;
 };
 
 const padWithZero = (value: number) => {
