076b8480a9272ea03ce37d7c1553535ff396d3a6
Darosss <daroing12@gmail.com>
2024-04-06 14:29:35 +0200

    feat(ProgressBar): add ProgressBar component

---
+++ src/components/common/progress-bar.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import styles from "./progress-bar.module.scss";
+import { Tooltip } from "react-tooltip";
+type ProgressBarProps = {
+  value: number;
+  maxValue: number;
+  valueName: string;
+  maxValueName: string;
+  showPercents?: boolean;
+  className?: string;
+};
+
+const TOOLTIP_ID = "progress-bar-tooltip-id";
+
+export const ProgressBar: FC<ProgressBarProps> = ({
+  value,
+  maxValue,
+  valueName,
+  maxValueName,
+  showPercents,
+  className,
+}) => {
+  return (
+    <div
+      className={`${styles.progressBarWrapper} ${className ? className : ""}`}
+    >
+      <progress value={value} max={maxValue} data-tooltip-id={TOOLTIP_ID} />
+      <Tooltip id={TOOLTIP_ID}>
+        <div>
+          {valueName}: {value}
+        </div>
+        <div>
+          {maxValueName}: {maxValue}
+        </div>
+      </Tooltip>
+      {showPercents ? (
+        <span>{((value / maxValue) * 100).toFixed(0)}%</span>
+      ) : null}
+    </div>
+  );
+};
